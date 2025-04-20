279f18b899bfa44e06eaf95acee60750555fef70
Alex Baroň <alexandr.baron3@gmail.com>
2025-04-20 09:51:52 +0200

    Finished author flow

--- src/AL/Routes/authorRoutes.js
+++ src/AL/Routes/authorRoutes.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const controller = require('../Controllers/authorController');
 
 router.get('/', controller.getAllAuthors);
+router.get('/:id', controller.getAuthorById);
+router.post('/', controller.createAuthor);
+router.delete('/delete/:id', controller.deleteAuthor)
+router.put('/update/:id', controller.updateAuthor)
 
 module.exports = router;
--- src/BL/authorService.js
+++ src/BL/authorService.js
@@ -1,5 +1,22 @@
+const { response, request } = require('express');
 const repository = require('../DAL/authorRepository');
 
 exports.getAuthors = () => {
     return repository.getAll();
 } 
+
+exports.getAuthorById = (id) => {
+    return repository.getById(id);
+} 
+
+exports.createAuthor = (data) => {
+    return repository.create(data);
+}
+
+exports.deleteAuthor = (id) => {
+    return repository.delete(id);
+}
+
+exports.updateAuthor = (id, data) => {
+    return repository.update(id, data);
+}
