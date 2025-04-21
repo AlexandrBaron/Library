69587eb8912a8cc496454442e74fce54b518592d
Alex Baroň <alexandr.baron3@gmail.com>
2025-04-21 10:37:52 +0200

    "Added logaction for logging activity"

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const bookRoutes = require('./src/AL/Routes/bookRoutes');
 const authorRoutes = require('./src/AL/Routes/authorRoutes');
 const userRoutes = require('./src/AL/Routes/userRoutes');
 const loanRoutes = require('./src/AL/Routes/loanRoutes');
-const genreRoutes = require('./src/AL/Routes/genreRoute');
-const reservationRoutes = require('./src/AL/Routes/reservationRoute');
+const genreRoutes = require('./src/AL/Routes/genreRoutes');
+const reservationRoutes = require('./src/AL/Routes/reservationRoutes');
 const fineRoutes = require('./src/AL/Routes/fineRoutes');
+const actionlogRoutes = require('./src/AL/Routes/actionlogRoutes');
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use('/loans', loanRoutes);
 app.use('/genre', genreRoutes);
 app.use('/reservation', reservationRoutes);
 app.use('/fines', fineRoutes);
+app.use('/actionlog', actionlogRoutes);
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
